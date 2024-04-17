74eb5d0e41e9ad956777a470faac84bd4f3ba589
Bruno <antunes0215@gmail.com>
2024-04-17 19:20:13 -0300

    Adicionada a função de calcular a idade

--- firstapp/script.js
+++ firstapp/script.js
@@ -1,2 +1,12 @@
-idade = 24
-alert(idade)
+//input
+nome = "Bruno"
+anoNascimento = 1998
+
+//processamento
+hoje = new Date()
+anoAtual = hoje.getFullYear()
+idade = anoAtual - anoNascimento
+mensagem = "Me chamo " + nome + " e tenho " +idade+ " anos"
+
+//output
+alert(mensagem)
